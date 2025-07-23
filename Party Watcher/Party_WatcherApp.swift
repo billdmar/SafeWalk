603758113bdffec994a7d722d6cbf50e3565f9d6
billdmar <billdmar@gmail.com>
2025-07-23 16:07:37 -0500

    Initial commit: Party Watcher SwiftUI app scaffold

---
+++ Party Watcher/Party_WatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Party_WatcherApp.swift
+//  Party Watcher
+//
+//  Created by Bill Mar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Party_WatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Party WatcherTests/Party_WatcherTests.swift
@@ -0,0 +1,16 @@
+//
+//  Party_WatcherTests.swift
+//  Party WatcherTests
+//
+//  Created by Bill Mar on 7/23/25.
+//
+
+import Testing
+
+struct Party_WatcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
